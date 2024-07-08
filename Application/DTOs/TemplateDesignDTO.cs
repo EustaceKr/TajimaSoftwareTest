7b4f4a8747e59280fc9043262e9d97e61725a000
Stathis Kravaritis <efstathios.kravaritis@gmail.com>
2024-07-08 23:55:08 +0300

    DTOs Start

---
+++ Application/DTOs/TemplateDesignDTO.cs
@@ -0,0 +1,17 @@
+﻿using Data.Context.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class TemplateDesignDTO
+    {
+        public int TemplateId { get; set; }
+        public Template Template { get; set; }
+        public int DesignId { get; set; }
+        public Design Design { get; set; }
+    }
+}
